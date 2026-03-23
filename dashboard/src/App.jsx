39b92cb487b0c00c43ad64a90bca907e7f5e3928
ForhaadM <forhaadmiah1@gmail.com>
2026-03-23 15:22:49 -0400

    Finished AC1-5

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,9 +1,17 @@
-import ChromeExtensionView from "./components/chromeExtensionView"
+import ChromeExtensionView from "./components/extension/chromeExtensionView"
+import DashboardLayout from "./components/extension/layout/DashboardLayout"
+import RiskScoreWidget from "./components/extension/widgets/RiskScoreWidget"
 
 export default function App() {
   return (
-    <div className="app">
-      <ChromeExtensionView />
+    <div style={{ display: 'flex', height: '100vh', backgroundColor: 'white' }}>
+      <div style={{ width: '306px', flexShrink: 0 }}>
+        <ChromeExtensionView />
+      </div>
+
+      <div style={{ flex: 1, marginLeft: '50px', marginTop: '10px' }}>
+        <RiskScoreWidget />
+      </div>
     </div>
   )
 }
