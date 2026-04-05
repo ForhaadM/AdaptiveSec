c930628a40928bd03e83064aec852d2873a518cf
ForhaadM <forhaadmiah1@gmail.com>
2026-04-05 12:18:29 -0400

    Finished AC1-6

--- backend/test_ws.py
+++ backend/test_ws.py
@@ -2,7 +2,7 @@ import asyncio
 import websockets
 
 async def t():
-    uri = "ws://localhost:8000/ws/v1/alerts/testuser?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0dXNlciIsImV4cCI6MTc3NDQ3OTcyNH0.F10D36y-u5J68btdWvqz69-XXqwk2IqjVGyHG0-b4Zo"
+    uri = "ws://localhost:8000/ws/v1/alerts/testuser?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0dXNlciIsImV4cCI6MTc3NDk5NzMyNn0.zIPJGDhHh-J1O8cS_-htVJPUXMlxvRZyUI25kBT2Kqo"
     async with websockets.connect(uri) as ws:
         print("connected")
         msg = await ws.recv()
